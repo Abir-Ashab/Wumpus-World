53e685b7b0dd0716cc361484ff2096f45522bfd1
Abir-Ashab <bsse1315@iit.du.ac.bd>
2024-12-23 10:41:26 +0600

    little changes in app.js

--- app.js
+++ app.js
@@ -4,4 +4,4 @@ var port = 5000;
 
 connect().use(serveStatic(__dirname)).listen(port);
 
-console.log('Running Wumpus World on http://localhost:', port);
+console.log('Running Wumpus World on http://localhost:'+port);
